6f3aece85318906256207ddca0e2e2eb0c0faffc
Leandro <leandroglez39@gmail.com>
2022-05-07 13:48:03 +0100

    demo for demands problem

--- app/app.py
+++ app/app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, make_response, jsonify
 from flask_cors import CORS
 import json
-from vrp import convert_dict_to_list, distace_between_coords, solve_vrp, openrouteservice_features, get_route, save_distance_matrix_cache, read_distance_matrix_cache_from_db, init_db_cache
+from vrp import convert_dict_to_list, distace_between_coords, solve_vrp, openrouteservice_features, get_route, save_distance_matrix_cache, read_distance_matrix_cache_from_db, init_db_cache, solve_vrp_fix
+
 import threading
+ 
 
 app = Flask(__name__)
 CORS(app)
@@ -74,6 +76,44 @@ def routes():
     else:
         return make_response(jsonify(var.message['error']['message']), var.status)
 
+@app.route('/test', methods=['POST'])
+def test():
+    event_data = request.json
+
+    dump_data = json.dumps(event_data)
+    
+    json_data = json.loads(dump_data) 
+
+    list_coors = convert_dict_to_list(json_data["coords"], flag=False)
+    
+    list_coors.append(list_coors[0])
+    
+    
+    list_demands = convert_dict_to_list(json_data["demands"])
+    list_demands.append(0)
+
+    TIME_WINDOWS_LOWER = json_data["lower_time_windows"]
+  
+
+    TIME_WINDOWS_UPPER = json_data["upper_time_windows"]
+    TIME_WINDOWS_UPPER[len(list_coors)-1] = 0
+
+    capacity = int(json_data["load_capacity"])
+
+    
+
+    dist_matrix = distace_between_coords(list_coors)
+
+        
+
+
+    pro = solve_vrp_fix(dist_matrix, list_demands, capacity,
+                    TIME_WINDOWS_LOWER, TIME_WINDOWS_UPPER)
+
+
+    return make_response(jsonify(get_route(list_coors,pro.best_routes)), 200)
+
+
 if __name__ == '__main__':
     print("Server Running app in port %s"%(PORT))
 
@@ -83,5 +123,7 @@ if __name__ == '__main__':
     hilo = threading.Thread(target=save_distance_matrix_cache, daemon=True)
     hilo.start()
 
-    app.run(host=HOST, port=PORT, debug=False)
+    app.run(host=HOST, port=PORT, debug=True)
     
+
+   
